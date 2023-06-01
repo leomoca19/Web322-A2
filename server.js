adb6475832e1ed2ccb0f19625fc7a47d54e62a4f
leomoca19 <leomoca19@gmail.com>
2023-06-01 01:37:12 -0400

    Cleaned comments and duplicated function

--- server.js
+++ server.js
@@ -27,65 +27,7 @@ app.get('/', (req, res) => {
     res.redirect('/about');
 });
 
-
-// Route for redirecting to "/about"
-app.get('/', (req, res) => {
-    res.redirect('/about');
-  });
-
-
 //Serving about.html
 app.get('/about', (req, res)=>{
     res.sendFile(`${__dirname}/views/about`);
 });
-
-/*
---const express = require('express');
---const app = express();
---const port = process.env.PORT || 8080;*/
-/*
-// Middleware for serving static files
-app.use(express.static('public'));*/
-
-/*// Route for redirecting to "/about"
-app.get('/', (req, res) => {
-  res.redirect('/about');
-});
-
-// Route for serving the "about.html" file
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/views/about.html');
-});*/
-
-/*
-// Start the server
-app.listen(port, () => {
-  console.log(`Express http server listening on port ${port}`);
-});*/
-
-
-/*
-var express = require("express");
-var app = express();
-var HTTP_PORT = process.env.PORT || 8080;*/
-/*
-// call this function after the http server starts listening for requests
-function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT);
-}*/
-
-/*
-// setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
-    res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
-});
-
-// setup another route to listen on /about
-app.get("/about", function(req,res){
-    res.send("<h3>About</h3>");
-});
-
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
-
-*/
