e229b438a9c36cd4e9f04f0c07daaf80ca05545c
leomoca19 <leomoca19@gmail.com>
2023-06-02 23:00:29 -0400

    Store service and server.js - Now working routes and objects retrieved

--- server.js
+++ server.js
@@ -4,22 +4,59 @@
 *  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Leonardo de la Mora Caceres Student ID: 152877205 Date: 01-June-2023
+*  Name: Leonardo de la Mora Caceres Student ID: 152877205 Date: 02-June-2023
 *
 *  Online (Cyclic) Link: https://busy-jade-hen-belt.cyclic.app/
 *
 ********************************************************************************/ 
 const express = require('express');
+const storeService = require('./store-service');
 const app = express();
 const port = process.env.PORT || 8080;
 
+
+storeService.initialize()
+  .then(() => {
+    // Initialization successful, start the server
+    app.listen(HTTP_PORT, () => {
+      console.log("Express http server listening on port", HTTP_PORT);
+    });
+
+    // Calls all items
+    storeService.getAllItems()
+      .then(items => {
+        console.log("All Items:", items);
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
+
+    // Calls published items
+    storeService.getPublishedItems()
+      .then(publishedItems => {
+        console.log("Published Items:", publishedItems);
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
+
+    // Calls categories
+    storeService.getCategories()
+      .then(categories => {
+        console.log("Categories:", categories);
+      })
+      .catch(error => {
+        console.log("Error:", error);
+      });
+  })
+  .catch(error => {
+    // Initialization failed, output the error to the console
+    console.error("Initialization Error:", error);
+  });
+
+
 // Middleware for automaticly serving static files in public
 app.use(express.static('public'));
-
-//Store return of listen if later closing the server explicitly: const server = app...
-const server = app.listen(port, () => {
-    console.log(`Express http server listening on port ${port}`);
-});
 
 //Redirecting to "/about"
 app.get('/', (req, res) => {
@@ -29,4 +66,47 @@ app.get('/', (req, res) => {
 //Serving about.html
 app.get('/about', (req, res)=>{
     res.sendFile(`${__dirname}/views/about.html`);
+});
+
+// Route for returning published items
+app.get("/shop", (req, res) => {
+    storeService.getPublishedItems()
+      .then(publishedItems => {
+        res.send(JSON.stringify(publishedItems));
+      })
+      .catch(error => {
+        res.status(500).send("Error: " + error);
+      });
+  });
+
+// Route for returning all items
+app.get("/items", (req, res) => {
+    storeService.getAllItems()
+      .then(items => {
+        res.send(JSON.stringify(items));
+      })
+      .catch(error => {
+        res.status(500).send("Error: " + error);
+      });
+  });
+
+// Route for returning all categories
+app.get("/categories", (req, res) => {
+    storeService.getCategories()
+    .then(Categories => {
+      res.send(JSON.stringify(Categories));
+    })
+    .catch(error => {
+      res.status(500).send("Error: " + error);
+    });
+});
+
+// 404 error- page not found
+app.use((req, res) => {
+    res.status(404).send("404 - Page not found");
+    });
+
+//Store return of listen if later closing the server explicitly: const server = app...
+const server = app.listen(port, () => {
+    console.log(`Express http server listening on port ${port}`);
 });
---
+++ store-service.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+const path = require('path');
+
+let itemsData = [];
+let categoriesData = [];
+
+
+function initialize() {
+    return new Promise((resolve, reject) => {
+
+        let itemsLoaded = false;
+        let categoriesLoaded = false;
+
+      fs.readFile(path.join(__dirname, 'data', 'items.json'), 'utf8', (err, itemsContent) => {
+        if (err) {
+          reject("Unable to read items file");
+          return;
+        }
+        try {
+          itemsData = JSON.parse(itemsContent);
+        } catch (error) {
+            reject("Unable to read items file");
+            return;
+        }
+        if(itemsLoaded && categoriesLoaded){
+          resolve();
+        }
+        fs.readFile(path.join(__dirname, 'data', 'categories.json'), 'utf8', (err, categoriesContent) => {
+        if (err) {
+            reject("Unable to read categories file");
+            return;
+        }
+        try {
+            categoriesData = JSON.parse(categoriesContent);
+        } catch (error) {
+            reject("Unable to read categories file");
+            return;
+        }
+        if(categoriesLoaded && itemsLoaded){
+            resolve();
+         }
+        });
+      });
+    });
+}
+
+
+//get all items + error handling
+function getAllItems(){
+    return new Promise((resolve,reject)=>{
+        if(itemsData.length>0){
+            resolve(itemsData);
+        } else {
+                reject("No results returned")
+            }
+        });
+    }
+    //get all published items + error handling
+    function getPublishedItems() {
+        return new Promise((resolve, reject) => {
+            const publishedItems = itemsData.filter(item => items.published);
+            if (publishedItems.length > 0) {
+                resolve(publishedItems);
+            } else {
+                reject("No results returned");
+            }
+        });
+    }
+    //get categories + error handling
+    function getCategories(){
+        return new Promise((resolve,reject)=> {
+            if(categoriesData.length>0){
+                resolve(categoriesData);
+            }else{
+                reject("No results returned")
+            }
+        });
+    }
+    
+    // Route functions
+        function allItems() {
+            return JSON.stringify(itemsData);
+        }
+    
+        function allCategories() {
+        return JSON.stringify(categoriesData);
+        }
+    
+module.exports = {
+    initialize,
+    getAllItems,
+    getPublishedItems,
+    getCategories,
+    allItems,
+    allCategories
+    };
+          
